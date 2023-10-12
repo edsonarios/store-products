fb5db373fbf0a5614ea785f65cfd895024e8e26e
edsonarios <edsonrios9@gmail.com>
2023-10-12 16:09:24 -0400

    Added pagination to get all products

--- back/routes/product.js
+++ back/routes/product.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router()
-const Product = require('../database')
+const Product = require('../models/database.js')
 
 router.get('/products', async (req, res, next) => {
     try {
-        const products = await Product.find()
-        res.status(200).json(products)
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 10
+
+        const skip = (page - 1) * limit
+        const total = await Product.countDocuments()
+
+        const products = await Product.find().skip(skip).limit(limit)
+
+        res.status(200).json({
+            total,
+            page,
+            totalPages: Math.ceil(total / limit),
+            limit,
+            data: products
+        })
     } catch (err) {
         next(err)
     }
@@ -35,16 +48,22 @@ router.post('/products', async (req, res) => {
 })
 
 router.put('/products/:id', async (req, res, next) => {
-    const { id } = req.params
+    const { id } = req.params.id
     try {
         const product = await Product.findById(id)
         if (!product) {
             return res.status(404).send({ message: `Product with id ${id} not found` })
         }
 
-        Object.keys(req.body).forEach(key => {
-            product[key] = req.body[key]
-        })
+        if (req.body.price && product.price !== req.body.price) {
+            product.priceHistory.push({ date: new Date(), price: req.body.price })
+            product.price = req.body.price
+        }
+
+        if (req.body.stock && product.stock !== req.body.stock) {
+            product.stockHistory.push({ date: new Date(), stock: req.body.stock })
+            product.stock = req.body.stock
+        }
 
         await product.save()
         res.status(200).json(product)
